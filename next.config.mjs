0cd9a52c25ed19cfe9fd37605cdf5b99b5b985e7
“attriato” <attriato@gmail.com>
2026-09-05 15:49:58 -0500

    Added purge-cache workflow and updated next.config.mjs with cache headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
       { source: "/data/:path*", destination: "/api/data/:path*" },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/_next/data/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, s-maxage=31536000",
+          },
+        ],
+      },
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, s-maxage=31536000",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
