b12b5b0cb15c433617a4f2bc4a2f88e513edfcc3
“attriato” <attriato@gmail.com>
2026-09-06 15:11:02 -0500

    Added source maps

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   outputFileTracingRoot: import.meta.dirname,
   async rewrites() {
     return [
